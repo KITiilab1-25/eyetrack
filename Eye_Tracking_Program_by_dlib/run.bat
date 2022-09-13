c96c4156c6097790be56c975167bb5802d828690
KITiilab1-25 <112159313+KITiilab1-25@users.noreply.github.com>
2022-09-13 22:18:25 +0900

    Update run.bat

--- Eye_Tracking_Program_by_dlib/run.bat
+++ Eye_Tracking_Program_by_dlib/run.bat
@@ -2,7 +2,7 @@ rem 実行したい.pyファイルがあるフォルダに移動
 cd ./
 rem 仮想環境の立ち上げ
 
-call C:\Users\ユーザー\Anaconda3\Scripts\activate.bat
+call C:\Users\EggOnRice\Anaconda3\Scripts\activate.bat
 call activate eyetrack
 rem .pyファイルの実行
 python eye_tracking.py
